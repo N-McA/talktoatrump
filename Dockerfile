1ecc98ca470e4db7cce9bbf3b9718ce87b482d83
nat <git@nat-mcaleese.co.uk>
2016-11-09 17:09:08 +0000

    added zak's sweet html

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ WORKDIR /app
 COPY app/package.json .
 RUN npm install
 COPY app/src src
+COPY app/static static
 CMD node src/app.js 
--- app/src/app.js
+++ app/src/app.js
@@ -1,11 +1,41 @@
 var express = require('express')
 var app = express()
+var WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({ port: 8080 });
 
-	app.get('/', function (req, res) {
-		  res.send('Hello World!!!!')
-	})
+
+// Express stuff
+// app.get('/', function (req, res) {
+//   res.send('Hello World!!!!')
+// })
+
+app.use(express.static('static'))
+
+app.get('/api/trump', function (req, res) {
+  res.send('Trump id')
+})
+
+app.get('/api/not', function (req, res) {
+  res.send('not trump id')
+})
 
 app.listen(80, function () {
 	  console.log('Example app listening on port 80!')
 })
+// end express
 
+// signalling server stuff
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    if (!msg.type) console.log(JSON.stringify(message));
+    switch (msg.type) {
+      case "signalling":
+
+    }
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
+// end signalling server
