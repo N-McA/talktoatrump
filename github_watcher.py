dd96e860231b35a47be3422f328267695668b230
nat <git@nat-mcaleese.co.uk>
2016-11-10 11:01:40 +0000

    added restart script...

--- github_watcher.py
+++ github_watcher.py
@@ -2,14 +2,26 @@
 
 from flask import Flask, request
 import json
+import subprocess
+
 
 app = Flask(__name__)
 
+def restart_server():
+  bashCommand = "./prod_restart"
+  process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+  output, error = process.communicate()
+  print output
+  print error
+
 @app.route('/',methods=['POST'])
 def foo():
-   data = json.loads(request.data)
-   print data
-   return "OK"
+  data = json.loads(request.data)
+  if data["pusher"]["name"] == "N-McA":
+    if data["ref"] == "refs/heads/production":
+      restart_server()
+  print data
+  return "OK"
 
 if __name__ == '__main__':
-   app.run(port=9999, host="0.0.0.0")
+  app.run(port=9999, host="0.0.0.0")
